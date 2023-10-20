1bb0660f45a0c30fa2f49ad1311d006106fbc670
Andrey Timoshenkov <andreytmshnkv@gmail.com>
2023-10-20 21:15:13 +0300

    feat: finish slicing for the Data comp

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TuiTextareaModule } from '@taiga-ui/kit';
+import { FormControl, FormGroup} from '@angular/forms';
+import { TuiButtonModule, TuiModeModule, TuiSvgModule, TuiTextfieldControllerModule } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-data',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TuiTextareaModule, FormsModule,
+    ReactiveFormsModule, TuiTextfieldControllerModule, TuiSvgModule, TuiModeModule, TuiTextfieldControllerModule, TuiButtonModule ],
   templateUrl: './data.component.html',
   styleUrls: ['./data.component.css']
 })
 export class DataComponent {
+  JSONform = new FormGroup({
+    JSON: new FormControl(''),
+  });
+
+  // saveData(click)
 
 }
