6d7123cad466a11cf750c678fa2b470e8eac7011
Andrey Timoshenkov <andreytmshnkv@gmail.com>
2023-10-26 18:53:37 +0300

    feat: edited data is saved and rerendered

--- src/app/projects/project-details/project-details.component.ts
+++ src/app/projects/project-details/project-details.component.ts
@@ -20,7 +20,6 @@ export class ProjectDetailsComponent implements OnChanges {
 
   @Input() project!: ProjectInterface | undefined;
   @Input() projects!: ProjectInterface[];
-  // @Input() currentProject!: ProjectInterface
 
 
   projectForm!: FormGroup;
@@ -29,31 +28,22 @@ export class ProjectDetailsComponent implements OnChanges {
   onEditClick() {
     this.editability = true;
     this.projectForm.enable();
-    console.log(this.project?.subject)
-    
-    // this.projectForm.disable()
-    // const elements = document.getElementsByClassName("value")
-    // for(let i = 0; i < elements.length; i++) {
-    //   elements[i].setAttribute("contenteditable", "true");
-    // }
-    // elements[0].focus();
   }
 
-  onSaveClick(project: ProjectInterface | undefined) {
+  onSaveClick() {
     this.editability = false;
-    this.projectForm.disable();
-    // const elements = document.getElementsByClassName("value")
-    // for(let i = 0; i < elements.length; i++) {
-    //   elements[i].setAttribute("contenteditable", "false")
-    // }
-    // this.localStore.saveData(project.id, JSON.stringify(project))
-    // console.log(project);
+    if (this.project !== undefined){
+      this.project.subject = this.projectForm.value.subject;
+      this.project.startDate = this.projectForm.value.startDate;
+      this.project.endDate = this.projectForm.value.endDate;
+      this.project.description = this.projectForm.value.description;
+      this.project.createdBy = this.projectForm.value.createdBy;
+      this.localStore.saveData(this.project?.id, JSON.stringify(this.project))
+      this.projects = this.localStore.getAllItems();
+    }
   }
-
-  constructor(private localStore: StorageService) {
-  
+  constructor(private localStore: StorageService) { 
 }
-
   ngOnChanges(changes: SimpleChanges) {
     if (typeof this.projectForm === 'undefined') {
       this.projectForm = new FormGroup({
@@ -63,11 +53,11 @@ export class ProjectDetailsComponent implements OnChanges {
         createdBy: new FormControl(),
         description: new FormControl()
       });
-      this.projectForm.disable()
+      this.projectForm.disable();
     }
     if (typeof changes['project'].currentValue !== 'undefined') {
       const {subject, startDate, endDate, createdBy, description} = changes['project'].currentValue;
-      this.projectForm.setValue({subject, startDate, endDate, createdBy, description})
+      this.projectForm.setValue({subject, startDate, endDate, createdBy, description});
     }
   }
 }
